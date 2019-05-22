0c54ba2de256756ae56cb2a913841371e7f92bb6
Alexey Korablin <Alexey_Korablin@epam.com>
2019-05-22 17:41:26 +0300

    02_0 Bencode (broken)

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
-const torrent = fs.readFileSync('./puppy.torrent');
-console.log(torrent.toString('utf8'));
+const bencode = require('bencode');
+
+// console.log(bencode);
+
+const torrent = bencode.decode(fs.readFileSync('./puppy.torrent'));
+console.log(torrent);
